c3ce567559615eb216dedb082f90fbcb9425c331
Timm Preetz <timm@preetz.us>
2018-06-16 11:21:44 +0200

    Add test for injected error (syntax still a little rough)

--- src/samples/simple-saga/__tests__/simpleSaga.test.ts
+++ src/samples/simple-saga/__tests__/simpleSaga.test.ts
@@ -1,7 +1,7 @@
 import { createTestContext } from '../../../lib';
 import { loadDataAction, saga } from '../sample01';
 
-test('simple saga test', async () => {
+test('simple saga test succes', async () => {
   const { ctx, isDone } = createTestContext({
     stubs: [
       {
@@ -19,4 +19,29 @@ test('simple saga test', async () => {
   expect(name).toBe('Bob');
 
   isDone();
+});
+
+test('simple saga test failure', async () => {
+  const noConnectionError = new Error(`No connection`);
+  const { ctx, isDone } = createTestContext({
+    stubs: [
+      {
+        function: window.fetch,
+        params: [`/api/user/5`],
+        result: Promise.reject(noConnectionError),
+      },
+    ],
+  });
+
+  try {
+    await saga(ctx, loadDataAction);
+  } catch (e) {
+    expect(e).toBe(noConnectionError);
+    isDone();
+
+    return;
+  }
+
+  // Should not be reached
+  expect(true).toBe(false);
 });
