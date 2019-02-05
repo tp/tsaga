e801d023cc12d2c4106e3d80ffaceaae157e930a
Timm Preetz <timm@preetz.us>
2019-02-05 09:39:35 +0100

    Make latest approach primary one

---
+++ old-samples/README.md
@@ -0,0 +1,1 @@
+Old usage example. Should be scavenged for what is not yet tested / sampled in the latest implementation. Rest should be discarded.
--- src/lib/index.ts
+++ src/lib/index.ts
@@ -1,70 +1,121 @@
-import { isEqual } from 'lodash';
+import { Middleware } from 'redux';
+import { Action as FSAAction, ActionCreator, isType } from 'typescript-fsa';
+import { CancellationToken } from './CancellationToken';
+import { Environment } from './environment';
+import { SagaCancelledError } from './SagaCancelledError';
+import { Action } from './types';
 
-export function call<R>(f: () => Promise<R>): Promise<R>;
-export function call<R, A>(f: (_0: A) => Promise<R>, _0: A): Promise<R>;
-export function call<R, A, B>(f: (_0: A, _1: B) => Promise<R>, _0: A, _1: B): Promise<R>;
-export function call(f: (...args: any[]) => Promise<any>, ...args: any[]): Promise<any> {
-  if (!f) {
-    throw new Error(`call: function to be called is undefined`);
+export { Task } from './environment';
+export { Action } from './types';
+export { Environment };
+export { testSaga, runs, selects } from './testHelpers';
+
+export interface Saga<StateT, ActionT extends Action, Payload> {
+  actionCreator: ActionCreator<Payload>;
+  saga: (ctx: Environment<StateT, ActionT>, action: FSAAction<Payload>) => Promise<void>;
+  type: 'every' | 'latest';
+}
+
+export function createTypedForEvery<StateT, ActionT extends Action>(): <Payload>(
+  actionCreator: ActionCreator<Payload>,
+  saga: (ctx: Environment<StateT, ActionT>, action: FSAAction<Payload>) => Promise<void>,
+) => Saga<StateT, ActionT, Payload> {
+  return (actionCreator, saga) => {
+    return {
+      actionCreator,
+      saga,
+      type: 'every',
+    };
+  };
+}
+
+export function createTypedForLatest<StateT, ActionT extends Action>(): <Payload>(
+  actionCreator: ActionCreator<Payload>,
+  saga: (ctx: Environment<StateT, ActionT>, action: FSAAction<Payload>) => Promise<void>,
+) => Saga<StateT, ActionT, Payload> {
+  return (actionCreator, saga) => {
+    return {
+      actionCreator,
+      saga,
+      type: 'latest',
+    };
+  };
+}
+
+export type AnySaga = Saga<any, any, any>;
+
+export function tsagaReduxMiddleware(sagas: AnySaga[]) {
+  // TODO: Remove completed sagas from this (currently leaks all results)
+  const sagaPromises: any = [];
+  const cancellationTokens = new Map<AnySaga, CancellationToken>();
+  let awaitingMessages: { actionCreator: ActionCreator<any>; promiseResolve: (action: any) => void }[] = [];
+
+  function waitForMessage<Payload>(actionCreator: ActionCreator<Payload>): Promise<FSAAction<Payload>> {
+    console.error(`waitForMessage called`);
+
+    return new Promise((resolve, reject) => {
+      awaitingMessages.push({ actionCreator: actionCreator, promiseResolve: resolve });
+      console.error(`awaitingMessages`, awaitingMessages);
+    });
   }
-  if (typeof f !== 'function') {
-    throw new Error(`call: function to be called is not a function`);
-  }
 
-  return f.call(window, ...args);
-}
+  const middleWare: Middleware = (api) => {
+    return function next(next) {
+      return function(action) {
+        next(action);
 
-export interface SagaContext {
-  call: typeof call;
-}
+        for (const config of awaitingMessages) {
+          if (isType(action, config.actionCreator)) {
+            config.promiseResolve(action);
+          }
+        }
+        awaitingMessages = awaitingMessages.filter((config) => !isType(action, config.actionCreator));
 
-/**
- * Context for live/production usage
- *
- * Executes everything unchanged
- */
-export const pureContext: SagaContext = {
-  call: call,
-};
+        for (const saga of sagas) {
+          if (isType(action, saga.actionCreator)) {
+            let cancellationToken;
+            if (saga.type === 'latest') {
+              const runningSagaCancellationToken = cancellationTokens.get(saga);
+              if (runningSagaCancellationToken) {
+                runningSagaCancellationToken.cancel();
+              }
 
-export interface TestContextResult<T extends SagaContext = SagaContext> {
-  ctx: T;
-  isDone: () => void;
-}
+              cancellationToken = new CancellationToken();
+              cancellationTokens.set(saga, cancellationToken);
+            }
 
-export interface TestContextConfiguration {
-  stubs: TestContextStub[];
-}
+            const context = new Environment(
+              api as any /* TODO: subscribe is missing, but that's fine for now */,
+              waitForMessage,
+              cancellationToken,
+            );
+            // console.error(`action matches expected creator`, action, `running saga`);
 
-interface TestContextStub {
-  function: (x: string) => Promise<any>;
-  params: [string] | [string, any];
-  result: any;
-}
+            sagaPromises.push(
+              saga
+                .saga(context, action)
+                .then((e) => 'completed')
+                .catch((e) => {
+                  if (e instanceof SagaCancelledError) {
+                    return 'cancelled';
+                  }
 
-export function createTestContext(config: TestContextConfiguration): TestContextResult {
-  const stubCall: typeof call = async (f: any, ...args: any[]): Promise<any> => {
-    const nextStub = config.stubs.shift();
-
-    if (!nextStub) {
-      throw new Error(`No more stubs available`);
-    } else if (nextStub.function !== f) {
-      throw new Error(`Other function than expected called`);
-    }
-
-    if (!isEqual(args, nextStub.params)) {
-      throw new Error(`Function parameters differ`); // TODO: Include helpful diff
-    }
-
-    return Promise.resolve(nextStub.result);
+                  console.error(`Saga failed`, e);
+                  return 'failed';
+                }),
+            );
+          }
+        }
+      };
+    };
   };
 
-  return {
-    ctx: { call: stubCall },
-    isDone: () => {
-      if (config.stubs.length !== 0) {
-        throw new Error(`Unused stubs left`);
-      }
-    },
+  const sagaCompletion = async (): Promise<void> => {
+    const promises = sagaPromises.slice(0);
+
+    const res = await Promise.all(promises);
+    console.error(`res`, res);
   };
+
+  return { middleware: middleWare, sagaCompletion };
 }
