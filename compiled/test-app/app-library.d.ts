506738b2b9c5c420984b497fec65cf73ac753d3e
Henri Beck <henribeck.dev@gmail.com>
2019-02-06 01:16:47 +0100

    Remove compiled dir from git

--- compiled/test-app/app-library.d.ts
+++
@@ -1,3 +0,0 @@
-import { User } from './types';
-export declare function sleep(timeoutMS: number): Promise<void>;
-export declare function loadUser(id: number): Promise<User>;
--- compiled/test-app/selectors.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getCount = function (state) { return state.count; };
-exports.getSelectedUserId = function (state) { return state.selectedUser; };
-exports.getUserById = function (state, id) { return state.usersById[id]; };
