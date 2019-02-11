bea926689e08a5e027820c7941b817a471a22c23
Henri Beck <henribeck.dev@gmail.com>
2019-02-11 10:50:27 +0100

    Added a type property to the TimeoutError

--- src/lib/TimeoutError.ts
+++ src/lib/TimeoutError.ts
@@ -1,6 +1,8 @@
 import { ActionCreator } from 'typescript-fsa';
 
 export default class TimeoutError extends Error {
+  public readonly type = 'timeout';
+
   constructor(actionCreator: ActionCreator<any>) {
     super(`An actions wasn't fired within the timeout: ${actionCreator.type}`);
   }
