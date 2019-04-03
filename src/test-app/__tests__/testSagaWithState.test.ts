fe1db4a9b11dd02abf2c19a60a7b8b4075d1dcce
Timm Preetz <timm@preetz.us>
2019-04-03 08:43:36 +0200

    Revive old test cases

--- src/test-app/__tests__/testSagaWithState.test.ts
+++ src/test-app/__tests__/testSagaWithState.test.ts
@@ -1,7 +1,6 @@
 // tslint:disable-next-line:no-implicit-dependencies (Just used in tests)
 import * as nock from 'nock';
-import { expectSaga } from '../../lib';
-import { select } from '../../lib/testing/mocks';
+import { calls, dispatches, selects, testSagaWithState } from '../../lib';
 import { setCount, userSelected } from '../actions';
 import { sleep } from '../app-library';
 import { userReducer } from '../reducers';
@@ -10,45 +9,49 @@ import { getCount } from '../selectors';
 
 nock.disableNetConnect();
 
-test('Test helper with mocked sleep call', () => {
-  return expectSaga(watchForUserSelectorToCountIfNotChangedWithing3s)
-    .withReducer(userReducer)
-    .toCall(sleep, 3000)
-    .dispatch(userSelected({ id: 2 }))
-    .toHaveFinalState({ count: 1, selectedUser: 2, usersById: {} })
-    .run();
+test('Test helper with mocked sleep call', async () => {
+  return testSagaWithState(
+    watchForUserSelectorToCountIfNotChangedWithing3s,
+    userSelected({ id: 2 }),
+    [calls(sleep).receiving()],
+    undefined,
+    userReducer,
+    { count: 1, selectedUser: 2, usersById: {} },
+  );
 });
 
 test('Test helper with mocked select', async () => {
-  return expectSaga(watchForUserSelectorToCountIfNotChangedWithing3s)
-    .withReducer(userReducer)
-    .withMocks([select(getCount, 5)])
-    .toCall(sleep, 3000)
-    .dispatch(userSelected({ id: 2 }))
-    .toHaveFinalState({ count: 6, selectedUser: 2, usersById: {} })
-    .run();
+  return testSagaWithState(
+    watchForUserSelectorToCountIfNotChangedWithing3s,
+    userSelected({ id: 2 }),
+    [calls(sleep).receiving(), selects(getCount).receiving(5)],
+    undefined,
+    userReducer,
+    { count: 6, selectedUser: 2, usersById: {} },
+  );
 });
 
 test('Test helper asserting on message', async () => {
-  return expectSaga(watchForUserSelectorToCountIfNotChangedWithing3s)
-    .withReducer(userReducer)
-    .withMocks([select(getCount, 5)])
-    .toCall(sleep, 3000)
-    .toDispatch(setCount({ count: 6 }))
-    .dispatch(userSelected({ id: 2 }))
-    .toHaveFinalState({ count: 6, selectedUser: 2, usersById: {} })
-    .run();
+  return testSagaWithState(
+    watchForUserSelectorToCountIfNotChangedWithing3s,
+    userSelected({ id: 2 }),
+    [calls(sleep).receiving(), selects(getCount).receiving(5), dispatches(setCount({ count: 6 }))],
+    undefined,
+    userReducer,
+    { count: 6, selectedUser: 2, usersById: {} },
+  );
 });
 
 test("Test helper asserting on message, fails of message doesn't match", async () => {
   try {
-    await expectSaga(watchForUserSelectorToCountIfNotChangedWithing3s)
-      .withReducer(userReducer)
-      .toCall(sleep, 3000)
-      .toDispatch(setCount({ count: 9999 }))
-      .dispatch(userSelected({ id: 2 }))
-      .toHaveFinalState({ count: 6, selectedUser: 2, usersById: {} })
-      .run();
+    await testSagaWithState(
+      watchForUserSelectorToCountIfNotChangedWithing3s,
+      userSelected({ id: 2 }),
+      [calls(sleep).receiving(), dispatches(setCount({ count: 9999 }))],
+      undefined,
+      userReducer,
+      { count: 6, selectedUser: 2, usersById: {} },
+    );
   } catch (e) {
     return;
   }
